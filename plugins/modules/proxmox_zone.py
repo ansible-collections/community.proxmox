0c0bb74bcf675c62dc96587b496114bfd4070a5e
Jana Hoch <janahoch91@proton.me>
2025-08-30 15:37:48 +0530

    proxmox_zone and proxmox module_utils
    - Move sdn locking functions to proxmox module_utils

--- plugins/modules/proxmox_zone.py
+++ plugins/modules/proxmox_zone.py
@@ -405,52 +405,6 @@ class ProxmoxZoneAnsible(ProxmoxAnsible):
                 changed=False, msg=zones
             )
 
-    def get_global_sdn_lock(self):
-        try:
-            return self.proxmox_api.cluster().sdn().lock().post()
-        except Exception as e:
-            self.module.fail_json(
-                msg=f'Failed to acquire global sdn lock {e}'
-            )
-
-    def apply_sdn_changes_and_release_lock(self, lock):
-        lock_params = {
-            'lock-token': lock,
-            'release-lock': 1
-        }
-        try:
-            return self.proxmox_api.cluster().sdn().put(**lock_params)
-        except Exception as e:
-            self.rollback_sdn_changes_and_release_lock(lock)
-            self.module.fail_json(
-                msg=f'Failed to apply sdn changes {e}. Rolling back all pending changes.'
-            )
-
-    def rollback_sdn_changes_and_release_lock(self, lock):
-        lock_params = {
-            'lock-token': lock,
-            'release-lock': 1
-        }
-        try:
-            self.proxmox_api.cluster().sdn().rollback().post(**lock_params)
-        except Exception as e:
-            self.module.fail_json(
-                msg=f'Rollback attempt failed - {e}. Manually clear lock by deleting /etc/pve/sdn/.lock'
-            )
-
-    def release_lock(self, lock):
-        lock_params = {
-            'lock-token': lock,
-            'force': 0
-        }
-        try:
-            self.proxmox_api.cluster().sdn().lock().delete(**lock_params)
-        except Exception as e:
-            self.module.fail_json(
-                msg=f'Failed to release lock - {e}. Manually clear lock by deleting /etc/pve/sdn/.lock'
-            )
-
-
     def get_zones(self, **type):
         print("reached")
         try:
