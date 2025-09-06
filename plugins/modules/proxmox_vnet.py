e48c9bc6d9ae8b570447562d15e7c484cd58507c
Jana Hoch <janahoch91@proton.me>
2025-09-06 14:01:04 +0530

    proxmox_vnet added docs
    - and fix Sanity issues

--- plugins/modules/proxmox_vnet.py
+++ plugins/modules/proxmox_vnet.py
@@ -9,13 +9,121 @@ from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 
-from pygments.lexer import default
+DOCUMENTATION = r"""
+module: proxmox_vnet
+short_description: Manage virtual networks in Proxmox SDN
+description:
+  - Create, update, or delete virtual networks in Proxmox SDN.
+  - Configure network isolation, VLAN awareness, and other network settings.
+author: 'Jana Hoch <janahoch91@proton.me> (!UNKNOWN)'
+attributes:
+  check_mode:
+    support: none
+  diff_mode:
+    support: none
+options:
+  state:
+    description:
+      - Desired state of the virtual network.
+      - Choices include present (create), absent (delete), or update (modify).
+    type: str
+    choices: ['present', 'absent', 'update']
+    default: present
+  force:
+    description:
+      - If true it will create vnet when state is update but vnet is missing and update the vnet when state is present and vnet already exists
+    type: bool
+    default: False
+  vnet:
+    description:
+      - The name of the virtual network to be managed.
+    type: str
+  zone:
+    description:
+      - zone for the virtual network.
+    type: str
+  alias:
+    description:
+      - An optional alias for the virtual network.
+    type: str
+  isolate_ports:
+    description:
+      - Enable isolation of ports within the virtual network.
+    type: bool
+    default: False
+  lock_token:
+    description:
+      - the token for unlocking the global SDN configuration.
+    type: str
+  tag:
+    description:
+      - tag for the virtual network.
+    type: int
+  type:
+    description:
+      - Type of network configuration.
+    type: str
+    choices: ['vnet']
+  vlanaware:
+    description:
+      - Enable VLAN awareness for the virtual network.
+    type: bool
+  delete:
+    description:
+      - A list of settings you want to delete.
+    type: str
+extends_documentation_fragment:
+  - community.proxmox.proxmox.actiongroup_proxmox
+  - community.proxmox.proxmox.documentation
+  - community.proxmox.attributes
+"""
 
-DOCUMENTATION = r""""""
 
-EXAMPLES = r""""""
+EXAMPLES = r"""
+- name: Create a vnet
+  community.proxmox.proxmox_vnet:
+    api_user: "{{ pc.proxmox.api_user }}"
+    api_token_id: "{{ pc.proxmox.api_token_id }}"
+    api_token_secret: "{{ vault.proxmox.api_token_secret }}"
+    api_host: "{{ pc.proxmox.api_host }}"
+    validate_certs: no
+    vnet: anstest
+    zone: ans1
+    state: present
 
-RETURN = r""""""
+- name: Update a vnet
+  community.proxmox.proxmox_vnet:
+    api_user: "{{ pc.proxmox.api_user }}"
+    api_token_id: "{{ pc.proxmox.api_token_id }}"
+    api_token_secret: "{{ vault.proxmox.api_token_secret }}"
+    api_host: "{{ pc.proxmox.api_host }}"
+    validate_certs: no
+    vnet: anstest
+    zone: ans1
+    alias: anst
+    state: update
+
+- name: Delete a vnet
+  community.proxmox.proxmox_vnet:
+    api_user: "{{ pc.proxmox.api_user }}"
+    api_token_id: "{{ pc.proxmox.api_token_id }}"
+    api_token_secret: "{{ vault.proxmox.api_token_secret }}"
+    api_host: "{{ pc.proxmox.api_host }}"
+    validate_certs: no
+    vnet: anstest
+    zone: ans1
+    state: absent
+"""
+
+RETURN = r"""
+vnet:
+  description:
+    - vnet name which was created/updated/deleted
+  returned: on success
+  type: str
+  sample:
+    anstest
+"""
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.community.proxmox.plugins.module_utils.proxmox import (
@@ -23,6 +131,7 @@ from ansible_collections.community.proxmox.plugins.module_utils.proxmox import (
     ansible_to_proxmox_bool,
     ProxmoxAnsible
 )
+
 
 def get_proxmox_args():
     return dict(
@@ -32,12 +141,13 @@ def get_proxmox_args():
         zone=dict(type="str", required=False),
         alias=dict(type="str", required=False),
         isolate_ports=dict(type="bool", default=False, required=False),
-        lock_token=dict(type="str", required=False),
+        lock_token=dict(type="str", required=False, no_log=False),
         tag=dict(type="int", required=False),
         type=dict(type="str", choices=['vnet'], required=False),
         vlanaware=dict(type="bool", required=False),
         delete=dict(type="str", required=False)
     )
+
 
 def get_ansible_module():
     module_args = proxmox_auth_argument_spec()
@@ -51,6 +161,7 @@ def get_ansible_module():
             ('state', 'absent', ['vnet'])
         ]
     )
+
 
 class ProxmoxVnetAnsible(ProxmoxAnsible):
     def __init__(self, module):
@@ -161,8 +272,9 @@ class ProxmoxVnetAnsible(ProxmoxAnsible):
                 self.module.warn(f'Failed to update vnet - {e}')
                 self.rollback_sdn_changes_and_release_lock(lock)
                 self.module.fail_json(
-                    msg=f'Failed to delete vnet - {e}. Rolling back all changes.'
+                    msg=f'Failed to delete vnet. Rolling back all changes - {e}'
                 )
+
 
 def main():
     module = get_ansible_module()
@@ -172,6 +284,7 @@ def main():
         proxmox.run()
     except Exception as e:
         module.fail_json(msg=f'An error occurred: {e}')
+
 
 if __name__ == "__main__":
     main()
