3302b8dfa7b3cbe09be63b25f0b0c8658bf5a173
nico <nicolas.vial@protonmail.com>
2025-12-22 10:21:11 +0100

    make cluster join idempotent

--- plugins/modules/proxmox_cluster.py
+++ plugins/modules/proxmox_cluster.py
@@ -50,6 +50,7 @@ options:
   cluster_name:
     description:
       - The cluster name to use for cluster creation.
+      - Not used when joining a cluster.
     type: str
     required: false
   state:
@@ -77,28 +78,26 @@ EXAMPLES = r"""
 
 - name: Join a Proxmox VE Cluster
   community.proxmox.proxmox_cluster:
-    state: present
     api_host: proxmoxhost
     api_user: root@pam
     api_password: password123
     master_ip: "{{ primary_node }}"
     fingerprint: "{{ cluster_fingerprint }}"
-    cluster_name: "devcluster"
 
 - name: Join a Proxmox VE Cluster with different API password
   community.proxmox.proxmox_cluster:
+    state: present
     api_host: proxmoxhost
     api_user: root@pam
     api_password: "{{ joining_node_api_password }}"
     master_api_password: "{{ master_node_api_password }}"
     master_ip: "{{ primary_node }}"
     fingerprint: "{{ cluster_fingerprint }}"
-    cluster_name: "devcluster"
 """
 
 RETURN = r"""
 cluster:
-  description: The name of the cluster that was created or joined.
+  description: The name of the cluster that was created.
   returned: success
   type: str
   sample: "devcluster"
@@ -112,18 +111,41 @@ from ansible_collections.community.proxmox.plugins.module_utils.proxmox import (
 
 
 class ProxmoxClusterAnsible(ProxmoxAnsible):
+    def check_is_cluster(self, cluster_status):
+        for d in cluster_status:
+            if d["type"] == "cluster":
+                return True
+        return False
+
+    def get_cluster_name(self, cluster_status):
+        for d in cluster_status:
+            if d["type"] == "cluster":
+                return d["name"]
+        return ""
+
+    def check_already_in_right_cluster(self, cluster_status, master_ip):
+        for d in cluster_status:
+            if ("ip" in d and d["ip"] == master_ip) or ("name" in d and d["name"] == master_ip):
+                return True
+        return False
+
     def cluster_create(self):
         cluster_name = self.module.params.get("cluster_name") or self.module.params.get("api_host")
         payload = {"clustername": cluster_name}
 
-        # Get cluster data
-        cluster_data = self.proxmox_api.cluster.config.totem.get()
-        # If we have data, check if we're already member of the desired cluster or a different one
-        if cluster_data and 'cluster_name' in cluster_data and cluster_data['cluster_name'] is not None:
-            if cluster_data['cluster_name'] == cluster_name:
-                self.module.exit_json(changed=False, msg="Cluster '{}' already present.".format(cluster_name), cluster=cluster_name)
+        cluster_status = self.proxmox_api.cluster.status.get()
+
+        if self.check_is_cluster(cluster_status):
+            if self.get_cluster_name(cluster_status) == cluster_name:
+                self.module.exit_json(
+                    changed=False,
+                    msg=f"Cluster '{cluster_name}' already present.",
+                    cluster=cluster_name
+                )
             else:
-                self.module.fail_json(msg='Error creating cluster: Node is already part of a different cluster - "{}"!'.format(cluster_data['cluster_name']))
+                self.module.fail_json(
+                    msg='Error creating cluster: Node is already part of a different cluster - "{}"!'.format(self.get_cluster_name(cluster_status))
+                )
 
         if self.module.params.get("link0") is not None:
             payload["link0"] = self.module.params.get("link0")
@@ -131,20 +153,27 @@ class ProxmoxClusterAnsible(ProxmoxAnsible):
             payload["link1"] = self.module.params.get("link1")
 
         if self.module.check_mode:
-            self.module.exit_json(changed=True, msg="Cluster '{}' would be created (check mode).".format(cluster_name), cluster=cluster_name)
-        else:
-            try:
-                self.proxmox_api.cluster.config.post(**payload)
-            except Exception as e:
-                self.module.fail_json(msg="Error while creating cluster: {}".format(str(e)))
+            self.module.exit_json(
+                changed=True,
+                msg=f"Cluster '{cluster_name}' would be created (check mode).",
+                cluster=cluster_name
+            )
 
-            self.module.exit_json(changed=True, msg="Cluster '{}' created.".format(cluster_name), cluster=cluster_name)
+        try:
+            self.proxmox_api.cluster.config.post(**payload)
+            self.module.exit_json(
+                changed=True,
+                msg=f"Cluster '{cluster_name}' created.",
+                cluster=cluster_name
+            )
+        except Exception as e:
+            self.module.fail_json(msg="Error while creating cluster: {}".format(str(e)))
 
     def cluster_join(self):
-        cluster_name = self.module.params.get("cluster_name")
         payload = {}
+        master_ip = self.module.params.get("master_ip")
 
-        payload["hostname"] = self.module.params.get("master_ip")
+        payload["hostname"] = master_ip
         payload["fingerprint"] = self.module.params.get("fingerprint")
         payload["password"] = self.module.params.get("master_api_password") or self.module.params.get("api_password")
 
@@ -152,32 +181,34 @@ class ProxmoxClusterAnsible(ProxmoxAnsible):
             payload["link0"] = self.module.params.get("link0")
         if self.module.params.get("link1") is not None:
             payload["link1"] = self.module.params.get("link1")
-        is_in_cluster = True
+
+        cluster_status = self.proxmox_api.cluster.status.get()
+
+        if self.check_is_cluster(cluster_status):
+            if self.check_already_in_right_cluster(cluster_status, master_ip):
+                self.module.exit_json(
+                    changed=False,
+                    msg="Node already in the cluster."
+                )
+
+            self.module.fail_json(
+                msg="Error while joining cluster: Node is already part of a cluster!"
+            )
 
         if self.module.check_mode:
-            cluster_objects = self.proxmox_api.cluster.status.get()
-            cluster_objects = cluster_objects[0]
-            cluster_id = cluster_objects.get("nodeid")
-
-            if cluster_id == 0:
-                is_in_cluster = False
-
-            if is_in_cluster:
-                self.module.fail_json(msg="Error while joining cluster: Node is already part of a cluster!")
-            else:
-                self.module.exit_json(changed=True, msg="Node would join the cluster '{}' (check mode).".format(cluster_name), cluster=cluster_name)
+            self.module.exit_json(
+                changed=True,
+                msg="Node would join the cluster (check mode)."
+            )
 
         try:
             self.proxmox_api.cluster.config.join.post(**payload)
+            self.module.exit_json(changed=True, msg="Node joined the cluster.")
 
         except Exception as e:
-            self.module.fail_json(msg="Error while joining the cluster: {}".format(str(e)))
-
-        self.module.exit_json(changed=True, msg="Node joined the cluster.", cluster=cluster_name)
-
-
-def proxmox_cluster_join_info_argument_spec():
-    return dict()
+            self.module.fail_json(
+                msg="Error while joining the cluster: {}".format(str(e))
+            )
 
 
 def validate_cluster_name(module, min_length=1, max_length=15):
@@ -187,7 +218,9 @@ def validate_cluster_name(module, min_length=1, max_length=15):
         module.fail_json(msg="Cluster name must be between {} and {} characters long.".format(min_length, max_length))
 
     if not re.match(r"^[a-zA-Z0-9\-]+$", cluster_name):
-        module.fail_json(msg="Cluster name must contain only letters, digits, or hyphens.")
+        module.fail_json(
+            msg="Cluster name must contain only letters, digits, or hyphens."
+        )
 
 
 def main():
@@ -216,15 +249,16 @@ def main():
     )
 
     proxmox = ProxmoxClusterAnsible(module)
-    validate_cluster_name(module)
 
     # The Proxmox VE API currently does not support leaving a cluster
     # or removing a node from a cluster. Therefore, we only support creating
-    # and joining a cluster. (https://pve.proxmox.com/pve-docs/api-viewer/#/cluster/config/nodes)
+    # and joining a cluster.
+    # (https://pve.proxmox.com/pve-docs/api-viewer/#/cluster/config/nodes)
     if module.params.get("state") == "present":
         if module.params.get("master_ip") and module.params.get("fingerprint"):
             cluster_action = proxmox.cluster_join()
         else:
+            validate_cluster_name(module)
             cluster_action = proxmox.cluster_create()
     else:
         cluster_action = {}
