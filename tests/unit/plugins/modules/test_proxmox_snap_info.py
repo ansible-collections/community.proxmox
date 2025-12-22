d3ec1b48cfad6fee8bd2d69f1aabb02c03592fb2
Tom Emming <tom@tinux-it.nl>
2025-12-22 07:27:39 +0100

    chore: added the missing copyright

--- tests/unit/plugins/modules/test_proxmox_snap_info.py
+++ tests/unit/plugins/modules/test_proxmox_snap_info.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+#
+# Copyright (c) 2019, Ansible Project
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
 from __future__ import (absolute_import, division, print_function)
 from ansible_collections.community.internal_test_tools.tests.unit.plugins.modules.utils import set_module_args
 import ansible_collections.community.proxmox.plugins.module_utils.proxmox as proxmox_utils
