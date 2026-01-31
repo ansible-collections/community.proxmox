37078dc43a612592dc82de5281647a8d01bf6a4d
IamLunchbox <r.grieger@hotmail.com>
2026-01-31 23:34:46 +0100

    apply ruff format and autofixes to proxmox.py

--- plugins/doc_fragments/proxmox.py
+++ plugins/doc_fragments/proxmox.py
@@ -2,11 +2,10 @@
 # GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from __future__ import (absolute_import, division, print_function)
-__metaclass__ = type
 
 
-class ModuleDocFragment(object):
+
+class ModuleDocFragment:
     # Common parameters for Proxmox VE modules
     DOCUMENTATION = r"""
 options:
