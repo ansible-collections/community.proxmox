7d85b67002599d1ef6c3ef68103ac8857ae4ade8
PendaGTP <38917281+PendaGTP@users.noreply.github.com>
2026-02-23 11:41:36 +0100

    refactor(acl): lint, create helper functions

--- plugins/modules/proxmox_access_acl.py
+++ plugins/modules/proxmox_access_acl.py
@@ -101,56 +101,84 @@ from ansible_collections.community.proxmox.plugins.module_utils.proxmox import (
 )
 
 
-class ProxmoxAccessACLAnsible(ProxmoxAnsible):
-    def _get(self):
-        acls = self.proxmox_api.access.acl.get()
-        return acls
+def _ace_matches(ace, desired):
+    if ace["path"] != desired["path"]:
+        return False
+    roleid = desired.get("roleid")
+    if roleid is not None and ace["roleid"] != roleid:
+        return False
+    ace_type = desired.get("type")
+    if ace_type is not None and ace["type"] != ace_type:
+        return False
+    ugid = desired.get("ugid")
+    if ugid is not None and ace["ugid"] != ugid:
+        return False
+    propagate = desired.get("propagate")
+    if propagate is not None:
+        ace_propagate = proxmox_to_ansible_bool(ace.get("propagate", 1))
+        if ace_propagate != propagate:
+            return False
+    return True
 
-    def _put(self, **data):
+
+def _build_put_payload(ace_data, delete=False):
+    payload = {
+        "path": ace_data["path"],
+        "roles": ace_data["roleid"],
+        "propagate": ace_data["propagate"],
+        f"{ace_data['type']}s": ace_data["ugid"],
+    }
+    if delete:
+        payload["delete"] = 1
+    return payload
+
+
+class ProxmoxAccessACLAnsible(ProxmoxAnsible):
+    def _get_acls(self):
+        return self.proxmox_api.access.acl.get()
+
+    def _put_acl(self, **data):
         return self.proxmox_api.access.acl.put(**data)
 
-    def create(self, acls, path, roleid, type, ugid, propagate):
-        for ace in acls:
-            ace_propagate = proxmox_to_ansible_bool(ace.get("propagate", 1))
-            if (ace["path"], ace["roleid"], ace["type"], ace["ugid"], ace_propagate) == (
-                path,
-                roleid,
-                type,
-                ugid,
-                propagate,
-            ):
-                return False
+    def _filter_matching_aces(self, existing_acls, desired):
+        return [ace for ace in existing_acls if _ace_matches(ace, desired)]
 
-        data = {"path": path, "roles": roleid, "propagate": ansible_to_proxmox_bool(propagate), f"{type}s": ugid}
+    def create(self, existing_acls, desired):
+        if self._filter_matching_aces(existing_acls, desired):
+            return False
 
-        self._put(**data)
+        payload = _build_put_payload(
+            {
+                "path": desired["path"],
+                "roleid": desired["roleid"],
+                "type": desired["type"],
+                "ugid": desired["ugid"],
+                "propagate": ansible_to_proxmox_bool(desired["propagate"]),
+            },
+            delete=False,
+        )
+        self._put_acl(**payload)
         return True
 
-    def delete(self, acls, path, roleid, type, ugid, propagate):
-        changed = False
-        for ace in acls:
-            if path != ace["path"]:
-                continue
-            if roleid and roleid != ace["roleid"]:
-                continue
-            if type and type != ace["type"]:
-                continue
-            if ugid and ace["ugid"] != ugid:
-                continue
-            ace_propagate = proxmox_to_ansible_bool(ace.get("propagate", 1))
-            if ace_propagate != propagate:
-                continue
+    def delete(self, existing_acls, desired):
+        to_remove = self._filter_matching_aces(existing_acls, desired)
 
-            data = {
-                "path": ace["path"],
-                "roles": ace["roleid"],
-                "propagate": ace["propagate"],
-                f"{ace['type']}s": ace["ugid"],
-            }
+        if not to_remove:
+            return False
 
-            self._put(**data, delete="1")
-            changed = True
-        return changed
+        for ace in to_remove:
+            payload = _build_put_payload(
+                {
+                    "path": ace["path"],
+                    "roleid": ace["roleid"],
+                    "type": ace["type"],
+                    "ugid": ace["ugid"],
+                    "propagate": ace.get("propagate", 1),
+                },
+                delete=True,
+            )
+            self._put_acl(**payload)
+        return True
 
 
 def run_module():
@@ -186,21 +214,22 @@ def run_module():
     state = module.params.get("state")
     path = module.params["path"]
     roleid = module.params["roleid"]
-    type = module.params["type"]
+    ace_type = module.params["type"]
     ugid = module.params["ugid"]
     propagate = module.params["propagate"]
 
     try:
-        result["old_acls"] = acls = proxmox._get()
+        result["old_acls"] = existing_acls = proxmox._get_acls()
+        desired_ace = dict(path=path, roleid=roleid, type=ace_type, ugid=ugid, propagate=propagate)
 
         if state == "present":
-            r = proxmox.create(acls, path, roleid, type, ugid, propagate)
+            r = proxmox.create(existing_acls, desired_ace)
         elif state == "absent":
-            r = proxmox.delete(acls, path, roleid, type, ugid, propagate)
+            r = proxmox.delete(existing_acls, desired_ace)
 
         result["changed"] = r
         if r:
-            result["new_acls"] = proxmox._get()
+            result["new_acls"] = proxmox._get_acls()
     except Exception as e:
         module.fail_json(msg=str(e), **result)
 
