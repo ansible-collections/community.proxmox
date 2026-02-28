823b60985ade98a3e147903a4fc8908d2b3b4cb8
PendaGTP <38917281+PendaGTP@users.noreply.github.com>
2026-02-28 10:13:40 +0100

    reverse changing default cephfs_options.path to prevent breaking change

--- plugins/modules/proxmox_storage.py
+++ plugins/modules/proxmox_storage.py
@@ -64,8 +64,8 @@ options:
       path:
         description:
           - The path to be used within the CephFS.
-          - The Proxmox default is '/mnt/pve/<storage_id>/<subdir>'.
         type: str
+        default: '/'
         required: false
       subdir:
         description:
@@ -489,7 +489,7 @@ def main():
                 "monhost": dict(type="list", elements="str"),
                 "username": dict(type="str"),
                 "password": dict(type="str", no_log=True),
-                "path": dict(type="str"),
+                "path": dict(type="str", default="/"),
                 "subdir": dict(
                     type="str",
                 ),
